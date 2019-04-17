0d7661188db4f9bad0af08c1baea98ebac70b201
anacamargos <alcvlara@sga.pucminas.br>
2019-04-17 17:38:40 -0300

    Criando casaToken

--- src/AnalisadorSintatico.java
+++ src/AnalisadorSintatico.java
@@ -19,6 +19,29 @@ public class AnalisadorSintatico {
     public InformacaoLexica getTokenLido() {
         return tokenLido;
     }
+
+    public void casaToken (InformacaoLexica tokenEsperado) throws Exception{
+        if (isEqual(tokenLido, tokenEsperado)) {
+
+            GerenciadorInput gi = new GerenciadorInput(tokenLido.lexema);
+            AnalisadorLexico al = new AnalisadorLexico(gi);
+            InformacaoLexica tokenLido = al.proximo();
+
+        } else {
+            //TODO gerar erro
+            System.out.println("Erro");
+        }
+    }
+
+    public boolean isEqual (InformacaoLexica tokenLido, InformacaoLexica tokenDesejado) {
+
+        boolean retorno = false;
+
+        if ( tokenLido.token == tokenDesejado.token && tokenLido.lexema.equals(tokenDesejado.lexema) && tokenLido.tipoConstante == tokenDesejado.tipoConstante) {
+            retorno = true;
+        }
+        return retorno;
+    }
 }
 
 /*
